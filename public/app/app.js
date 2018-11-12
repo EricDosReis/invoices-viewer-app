50b537cb562362e0a933d95b44c1c85a2b2776c2
Eric dos Reis Santos <eric.santos@SHX0532.shxinfor.local>
2018-11-12 17:04:27 -0200

    2nd chapter - Array.reduce

--- public/app/app.js
+++ public/app/app.js
@@ -5,5 +5,8 @@ document
   .onclick = () =>
     fetch('http://localhost:3000/invoices')
       .then(handleStatus)
+      .then(invoices => invoices.reduce((accumulator, invoice) => accumulator.concat(invoice.items), []))
+      .then(items => items.filter(item => item.id == '2143'))
+      .then(items => items.reduce((total, item) => total + item.value, 0))
       .then(console.log)
       .catch(console.log);
