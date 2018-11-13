340ef69b015a6c5ea816cab312741c97c677beb4
Eric dos Reis Santos <eric.santos@SHX0532.shxinfor.local>
2018-11-13 17:22:22 -0200

    4th chapter

--- public/app/app.js
+++ public/app/app.js
@@ -1,13 +1,22 @@
-import { handleStatus }  from './utils/fetch.js';
 import { log } from './utils/log.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 import './utils/array.js';
 
 import { invoiceService as service } from './invoice/service.js';
 
+const operations = pipe(
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500)
+);
+
+const action = operations(() =>
+  service
+    .sumItems('2143')
+    .then(log)
+    .catch(log)
+);
+
 document
   .querySelector('#fetchButton')
-  .onclick = () =>
-    service
-      .sumItems('2143')
-      .then(log)
-      .catch(log);
+  .onclick = action;
+
